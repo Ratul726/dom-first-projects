61aaa2c861be171a462e69bc5056746c9451408c
Ratul726 <ratulx726492@gmail.com>
2024-02-13 17:22:20 -0800

    second commit

---
+++ traiangel.js
@@ -0,0 +1,14 @@
+// console.log('hello world')
+// function calculateTraianleArea (){
+//     //console.log('Insite the Function')
+//     document.getElementById('triangel-base').addEventListener('click',function(){
+//         console.log('hello world')
+//     })
+// }
+
+function calculateTraianleArea () {
+    let triangelBaseInput = document.getElementById('triangel-base');
+    let triangelBaseText = triangelBaseInput.value;
+    // const bese = parseInt(triangelBaseText)
+    console.log(triangelBaseText ,'hello')
+}
